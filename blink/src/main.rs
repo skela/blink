65e50331ae53a4a41faed46735fc5ee9c44b579f
Alek Slater <skela@davincium.com>
2022-08-31 23:20:02 +0200

    editor config loads ok now

--- blink/src/main.rs
+++ blink/src/main.rs
@@ -1,9 +1,12 @@
 use clap::Parser;
+use ec4rs::property::IndentStyle;
+use std::path::Path;
 
 fn main()
 {
 	let args = Arguments::parse();
 	let res = std::fs::read_to_string(&args.path);
+	let config = load_config(&args.path.as_path());
 
 	match res
 	{
@@ -20,6 +23,48 @@ fn main()
 			println!("Error: Unable to read file `{}`\nReason: {}",args.path.display(),error);
 		}
 	}
+}
+
+fn load_config(path:&Path) -> Config
+{
+	let default_config = Config
+	{
+		indent_style: IndentStyle::Tabs,
+		indent_size: 2,
+		curly_bracket_next_line : true,
+	};
+
+	let res = ec4rs::properties_of(path);
+
+	match res
+	{
+		Ok(cfg) =>
+		{
+			let indent_style : IndentStyle = cfg.get::<IndentStyle>().unwrap_or(default_config.indent_style);
+			let indent_size = cfg.get_raw_for_key("indent_size").into_str().parse::<u8>().unwrap_or(default_config.indent_size);
+			let curly_bracket_next_line = cfg.get_raw_for_key("curly_bracket_next_line").into_str().parse::<bool>().unwrap_or(default_config.curly_bracket_next_line);
+			return Config
+			{
+				indent_style,
+				indent_size,
+				curly_bracket_next_line,
+			};
+		}
+
+		Err(error) =>
+		{
+			println!("Warning: Unable to load editorconfig - Resorting to defaults\nReason: {}",error);
+		}
+	}
+
+	return default_config;
+}
+
+struct Config
+{
+	indent_style : IndentStyle,
+	indent_size : u8,
+	curly_bracket_next_line : bool
 }
 
 #[derive(Parser)]
