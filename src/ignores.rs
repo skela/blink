f6f092ead054c9f35dad2ff170d1d7a9c2ecef8c
Alek Slater <skela@davincium.com>
2024-04-02 22:17:23 +0200

    search for and read .blinkignore files, stop at .git folder or root, whichever comes first

--- src/ignores.rs
+++ src/ignores.rs
@@ -1,8 +1,55 @@
+use std::fs;
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
 use std::{collections::HashSet, path::PathBuf};
 
-// TODO: Need to check current location, and navigate upwards until you reach either .git or
-// .blinkignore, whichever comes first
 pub(crate) fn load_ignores(path: &PathBuf) -> HashSet<PathBuf>
 {
-	return HashSet::new();
+	let mut files: HashSet<PathBuf> = HashSet::new();
+	if let Some(blinkignore) = find_blinkignore(path)
+	{
+		if let Ok(file) = File::open(&blinkignore)
+		{
+			let reader = io::BufReader::new(file);
+
+			for res in reader.lines()
+			{
+				if let Ok(line) = res
+				{
+					files.insert(PathBuf::from(line));
+				}
+			}
+		}
+	}
+	return files;
+}
+
+fn find_blinkignore(starting_path: &Path) -> Option<PathBuf>
+{
+	let mut current_path = starting_path.to_path_buf();
+
+	while current_path.as_os_str().len() > 0
+	{
+		let blinkignore_path = current_path.join(".blinkignore");
+		if fs::metadata(&blinkignore_path).is_ok()
+		{
+			return Some(blinkignore_path);
+		}
+
+		let git_path = current_path.join(".git");
+		if git_path.is_dir()
+		{
+			println!("Found .git directory at: {:?}", git_path);
+			return None;
+		}
+
+		match current_path.parent()
+		{
+			Some(parent) => current_path = parent.to_path_buf(),
+			None => return None, // If no parent, we're at the root
+		}
+	}
+
+	None
 }
